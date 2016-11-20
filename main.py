764bf01ae68c08dd97ff41da1f9e93a76a2ed985
vinjn <vinjn.z@gmail.com>
2016-11-20 20:41:40 +0800

    Adding osc features

--- main.py
+++ main.py
@@ -1,50 +1,58 @@
 #!/usr/bin/env python3
+'''
+main.py
+'''
 
 # NOTE: this example requires PyAudio because it uses the Microphone class
 
 import argparse
 import speech_recognition as sr
-from pythonosc import dispatcher, osc_server
+from pythonosc import udp_client
 
-if __name__ == "__main__":
+def main():
+    '''
+    main()
+    '''
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ip",
-        default="127.0.0.1", help="The ip to listen on")
-    parser.add_argument("--port",
-        type=int, default=3000, help="The port to listen on")
+    parser.add_argument("--ip", default="127.0.0.1",
+                        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=3000,
+                        help="The port the OSC server is listening on")
     args = parser.parse_args()
 
-    # server = osc_server.ThreadingOSCUDPServer(
-    #     (args.ip, args.port), dispatcher)
-    # print("Serving on {}".format(server.server_address))
-    # server.serve_forever()
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
 
-    r = sr.Recognizer()
-    m = sr.Microphone()
+    rec = sr.Recognizer()
+    mic = sr.Microphone()
 
     try:
         print("A moment of silence, please...")
-        with m as source: r.adjust_for_ambient_noise(source)
-        print("Set minimum energy threshold to {}".format(r.energy_threshold))
+        with mic as source:
+            rec.adjust_for_ambient_noise(source)
+        print("Set minimum energy threshold to {}".format(rec.energy_threshold))
         while True:
             print("Say something!")
-            with m as source: audio = r.listen(source)
+            with mic as source:
+                audio = rec.listen(source)
             print("Got it! Now to recognize it...")
             try:
                 # recognize speech using Google Speech Recognition
                 # value = r.recognize_google(audio)
-                value = r.recognize_bing(
+                value = rec.recognize_bing(
                     audio, key="0211831985124fdbb41fe2161bc1cd10", language="zh-CN")
 
                 # we need some special handling here to correctly print unicode
                 # characters to standard output
                 if str is bytes: # this version of Python uses bytes for strings (Python 2)
-                    print(u"You said {}".format(value).encode("utf-8"))
-                else: # this version of Python uses unicode for strings (Python 3+)
-                    print("You said {}".format(value))
+                    value = u"{}".format(value).encode("utf-8")
+                print("You said", value)
+                client.send_message("/say", value)
             except sr.UnknownValueError:
                 print("Oops! Didn't catch that")
-            except sr.RequestError as e:
-                print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+            except sr.RequestError as err:
+                print("Uh oh! Couldn't request results from; {0}".format(err))
     except KeyboardInterrupt:
         pass
+
+if __name__ == "__main__":
+    main()
